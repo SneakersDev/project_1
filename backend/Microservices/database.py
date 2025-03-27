e6a88bda3b101c6c1acb2801e8e5589de4886655
Luiskeo <luigicordoba903@gmail.com>
2025-03-26 22:59:36 -0500

    Update microservice (#47)

--- backend/Microservices/database.py
+++ backend/Microservices/database.py
@@ -1,6 +1,16 @@
 #Usa el patrón Singleton para evitar múltiples conexiones a MySQL.
 import mysql.connector
+from dotenv import load_dotenv
+import os
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+
+load_dotenv(dotenv_path)
+
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
 class DatabaseConnection:
     """Clase Singleton para manejar la conexión a MySQL."""
     _instance = None
@@ -9,11 +19,10 @@ class DatabaseConnection:
         if cls._instance is None:
             cls._instance = super(DatabaseConnection, cls).__new__(cls)
             cls._instance.connection = mysql.connector.connect(
-                host="localhost",
-                port=3306,
-                user="root",
-                password="1234",
-                database="sneakers"
+                host=db_host,
+                user=db_user,
+                password=db_password,
+                database=db_name
             )
         return cls._instance
 
